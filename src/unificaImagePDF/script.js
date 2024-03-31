c41e9032f92b9ec9db3d804c0c070f718bbc2c18
Marcelo Medeiros dos Santos <89617160+MarceloMederi@users.noreply.github.com>
2024-03-31 14:57:35 -0300

    Update script.js

--- src/unificaImagePDF/script.js
+++ src/unificaImagePDF/script.js
@@ -1,63 +1,92 @@
-document.getElementById('mergeBtn').addEventListener('click', async () => {
-    const fileInput = document.getElementById('fileInput');
-    const files = Array.from(fileInput.files); // Transforma FileList em um array
-
-    if (files.length < 2) {
-        alert('Selecione pelo menos 2 arquivos para mesclar.');
-        return;
-    }
-
-    const pdfDoc = await PDFLib.PDFDocument.create();
-
+document.getElementById('mergeBtn').addEventListener('click', async function () {
     try {
-        let progress = 0;
-        const progressBar = document.getElementById('progressBar');
-        const progressBarContainer = document.getElementById('progressBarContainer');
-        const PAGE_WIDTH = 1200; // Largura fixa da página
-        const PAGE_HEIGHT = 1200; // Altura fixa da página
-        const IMAGE_HEIGHT = 800; // Altura desejada para as imagens
+        // Obtém o input de arquivo e os arquivos selecionados
+        var fileInput = document.getElementById('fileInput');
+        var files = Array.from(fileInput.files);
 
-        for (let i = 0; i < files.length; i++) { // Percorre os arquivos na ordem original de seleção
-            const file = files[i];
-            const reader = new FileReader();
-            const dataUrl = await new Promise((resolve) => {
-                reader.onload = (event) => resolve(event.target.result);
+        // Verifica se há pelo menos 2 arquivos selecionados
+        if (files.length < 2) {
+            alert('Selecione pelo menos 2 arquivos para mesclar.');
+            return;
+        }
+
+        // Cria um novo documento PDF
+        var pdfDoc = await PDFLib.PDFDocument.create();
+
+        // Configurações para a barra de progresso
+        var progress = 0;
+        var progressBar = document.getElementById('progressBar');
+        var progressBarContainer = document.getElementById('progressBarContainer');
+
+        // Constantes para as dimensões da página
+        var PAGE_WIDTH = 1200;
+        var PAGE_HEIGHT = 1200;
+
+        // Loop através de todos os arquivos selecionados
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+
+            // Leitura dos dados do arquivo como Data URL
+            var reader = new FileReader();
+            var dataUrl = await new Promise(function (resolve) {
+                reader.onload = function (event) {
+                    return resolve(event.target.result);
+                };
                 reader.readAsDataURL(file);
             });
 
+            // Verifica o tipo do arquivo
             if (file.type === 'application/pdf') {
-                const existingPdfBytes = await fetch(dataUrl).then(response => response.arrayBuffer());
-                const existingPdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
-                const pages = await pdfDoc.copyPages(existingPdfDoc, existingPdfDoc.getPageIndices());
-                pages.forEach((page) => {
+                // Carrega o PDF e adiciona suas páginas ao documento PDF mesclado
+                var existingPdfBytes = await fetch(dataUrl).then(function (response) {
+                    return response.arrayBuffer();
+                });
+                var existingPdfDoc = await PDFLib.PDFDocument.load(existingPdfBytes);
+                var pages = await pdfDoc.copyPages(existingPdfDoc, existingPdfDoc.getPageIndices());
+                pages.forEach(function (page) {
                     pdfDoc.addPage(page);
                 });
             } else if (file.type === 'image/jpeg' || file.type === 'image/png') {
-                const img = new Image();
+                // Manipulação de imagens: redimensionamento e adição ao PDF
+                var img = new Image();
                 img.src = dataUrl;
-                await new Promise((resolve, reject) => {
-                    img.onload = () => resolve();
-                    img.onerror = (error) => reject(error);
+                await new Promise(function (resolve, reject) {
+                    img.onload = function () {
+                        return resolve();
+                    };
+                    img.onerror = function (error) {
+                        return reject(error);
+                    };
                 });
 
-                const page = pdfDoc.addPage([PAGE_WIDTH, PAGE_HEIGHT]); // Cria uma página com dimensões fixas
-                let embedImage;
+                var pageWidth = img.width > img.height ? PAGE_WIDTH : PAGE_HEIGHT * (img.width / img.height);
+                var pageHeight = img.height > img.width ? PAGE_HEIGHT : PAGE_WIDTH * (img.height / img.width);
+
+                var page = pdfDoc.addPage([pageWidth, pageHeight]);
+                var canvas = document.createElement('canvas');
+                var ctx = canvas.getContext('2d');
+
+                canvas.width = pageWidth;
+                canvas.height = pageHeight;
+
+                ctx.drawImage(img, 0, 0, pageWidth, pageHeight);
+
+                var resizedDataUrl = canvas.toDataURL('image/jpeg');
+
+                var embedImage;
                 if (file.type === 'image/jpeg') {
-                    embedImage = await pdfDoc.embedJpg(dataUrl);
+                    embedImage = await pdfDoc.embedJpg(resizedDataUrl);
                 } else if (file.type === 'image/png') {
-                    embedImage = await pdfDoc.embedPng(dataUrl);
+                    embedImage = await pdfDoc.embedPng(resizedDataUrl);
                 }
 
-                const scaleFactor = Math.min(PAGE_WIDTH / img.width, PAGE_HEIGHT / img.height); // Calcula a escala para caber na página
-                const scaledWidth = img.width * scaleFactor;
-                const scaledHeight = img.height * scaleFactor;
-
+                // Desenha a imagem no documento PDF
                 page.drawImage(embedImage, {
-                    x: (PAGE_WIDTH - scaledWidth) / 2, // Centraliza a imagem na página
-                    y: (PAGE_HEIGHT - scaledHeight) / 2,
-                    width: scaledWidth,
-                    height: scaledHeight,
-                    preserveAspectRatio: true // Mantém a proporção da imagem
+                    x: 0,
+                    y: 0,
+                    width: pageWidth,
+                    height: pageHeight,
+                    preserveAspectRatio: true
                 });
             } else {
                 alert('Formato de arquivo não suportado: ' + file.type);
@@ -65,30 +94,33 @@ document.getElementById('mergeBtn').addEventListener('click', async () => {
 
             // Atualiza a barra de progresso
             progress = ((i + 1) / files.length) * 100;
-            progressBar.style.width = `${progress}%`;
+            progressBar.style.width = progress + '%';
         }
 
-        const mergedPdfBytes = await pdfDoc.save();
-        const blob = new Blob([mergedPdfBytes], { type: 'application/pdf' });
-        const url = URL.createObjectURL(blob);
-        const output = document.getElementById('output');
-        output.innerHTML = ''; // Limpar qualquer conteúdo anterior
-        const link = document.createElement('a');
+        // Salva o documento PDF mesclado
+        var mergedPdfBytes = await pdfDoc.save();
+        var blob = new Blob([mergedPdfBytes], { type: 'application/pdf' });
+        var url = URL.createObjectURL(blob);
+
+        // Cria um link para download do PDF mesclado
+        var output = document.getElementById('output');
+        output.innerHTML = ''; // Limpa qualquer conteúdo anterior
+        var link = document.createElement('a');
         link.href = url;
         link.textContent = 'Download PDF Mesclado';
         link.setAttribute('download', 'merged.pdf');
         output.appendChild(link);
 
-        // Remover o botão de download após o download
-        link.addEventListener('click', () => {
+        // Remove o botão de download após o download
+        link.addEventListener('click', function () {
             output.removeChild(link);
         });
 
-        // Limpar a lista de seleção de arquivos de forma assíncrona
-        setTimeout(() => {
+        // Limpa a lista de seleção de arquivos de forma assíncrona
+        setTimeout(function () {
             fileInput.value = null;
         }, 100);
-        
+
         // Esconde a barra de progresso após o download
         progressBarContainer.style.display = 'none';
     } catch (error) {
